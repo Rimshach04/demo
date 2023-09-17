745b891cca8936084214a925563775da5a8289c1
Rimshach04 <136222947+Rimshach04@users.noreply.github.com>
2023-09-17 10:31:34 +0500

    Update Aaa.jsx

--- src/components/Aaa.jsx
+++ src/components/Aaa.jsx
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 function Aaa(){
    return(
     <div>
+       <h1>Hello React app</h1>
       <Button variant="primary">Primary</Button>{' '}
       <Button variant="secondary">Secondary</Button>{' '}
       <Button variant="success">Success</Button>{' '}
